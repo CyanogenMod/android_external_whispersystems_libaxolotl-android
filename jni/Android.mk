fa92d2063fe569a40f3d05d083ac4a06b3df0ce0
Adnan Begovic <adnan@cyngn.com>
2015-05-07 15:29:04 -0700

    libaxolotl: Set up libaxolotl module cmake build.
    Change-Id: I93fe5cc06f7fad11fbc6e3d194bc0d4ae1542c9a

--- jni/Android.mk
+++ jni/Android.mk
@@ -9,9 +9,26 @@ include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
+ed_25519_sources := $(wildcard $(LOCAL_PATH)/ed25519/*.c)
+ed_25519_sources := $(ed_25519_sources:$(LOCAL_PATH)/ed25519/%=%)
+
+ed_25519_additions_sources := $(wildcard $(LOCAL_PATH)/ed25519/additions/*.c)
+ed_25519_additions_sources := $(ed_25519_additions_sources:$(LOCAL_PATH)/ed25519/additions/%=%)
+
+ed_25519_nacl_sources := $(wildcard $(LOCAL_PATH)/ed25519/nacl_sha512/*.c)
+ed_25519_nacl_sources := $(ed_25519_nacl_sources:$(LOCAL_PATH)/ed25519/nacl_sha512/%=%)
+
 LOCAL_MODULE     := libcurve25519-ref10
-LOCAL_SRC_FILES  := $(wildcard ed25519/*.c) $(wildcard ed25519/additions/*.c) $(wildcard ed25519/nacl_sha512/*.c)
-LOCAL_C_INCLUDES := ed25519/nacl_includes ed25519/additions ed25519/sha512 ed25519
+LOCAL_SRC_FILES  := \
+    $(ed_25519_sources:%=ed25519/%) \
+    $(ed_25519_additions_sources:%=ed25519/additions/%) \
+    $(ed_25519_nacl_sources:%=ed25519/nacl_sha512/%)
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/ed25519/nacl_includes \
+    $(LOCAL_PATH)/ed25519/additions \
+    $(LOCAL_PATH)/ed25519/sha512 \
+    $(LOCAL_PATH)/ed25519
 
 include $(BUILD_STATIC_LIBRARY)
 
